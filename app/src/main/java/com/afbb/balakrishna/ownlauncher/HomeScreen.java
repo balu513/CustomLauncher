3c214ddd5c5b95258cbe72a4d5a1f3a8dacd3d1c
balakrishna <balub513@gmail.com>
2015-08-25 13:27:50 +0530

    commit

---
+++ app/src/main/java/com/afbb/balakrishna/ownlauncher/HomeScreen.java
@@ -0,0 +1,14 @@
+package com.afbb.balakrishna.ownlauncher;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+public class HomeScreen extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.homescreen);
+    }
+
+}
